39f5f1032c160f3c76aae63134605b0a2319a751
flordiprimo <flordiprimo@gmail.com>
2022-04-21 16:45:49 -0300

    entrega

--- src/components/Component404.jsx
+++ src/components/Component404.jsx
@@ -5,7 +5,7 @@ export default function Component404() {
   return (
     <>    
       <div className='container px-4 mx-auto justify-center flex flex-col'>
-          <img src='../images/404.png' width='600px' className='place-self-center ml-4'alt="illustration of a man shopping in an online store"/>
+          <img src='https://firebasestorage.googleapis.com/v0/b/ecommerce-diprimo.appspot.com/o/404.png?alt=media&token=bdb5f5e4-9147-4b93-ad84-f2ce3398fecb' width='600px' className='place-self-center ml-4'alt="illustration of a man shopping in an online store"/>
           <h2 className='text-center m-4 text-2xl'>¡Uy! Algo salió mal</h2>
           <p className='text-center'>La página que buscás no existe</p>
           <Link to={'/'} className='container px-4 mx-auto flex flex-col justify-center'>
